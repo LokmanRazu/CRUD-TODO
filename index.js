b221018b07bbfc72076406576b73d47dc186833c
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-03-07 01:29:09 +0600

    Deafult Error Handler

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/',(req,res)=>{
     res.send('hello World')
 })
 
+// Default Error handler
+function errorHandler(err,req,res,next){
+    if(res.headerSent){
+        return next(err)
+    }
+    res.status(500).json({error:err})
+}
+
 
 const PORT = process.env.PORT ||8080;
 mongoose.connect(`mongodb+srv://TestWLWS:9Nq4s6JbUZBrtC2R@cluster0.pnxzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
